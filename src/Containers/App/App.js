58cf503809b59f32660f538f39365ce3e9b5c0ab
ibraveboy <hamza.butt32@hotmail.com>
2019-11-06 12:51:42 +0500

    App.js modified

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Passion. In coding or development you only need to have passion.
         </a>
       </header>
     </div>
